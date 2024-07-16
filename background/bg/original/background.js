89213226e73886ef32626a95dd9a64e55ad6d5e4
HonkW93 <contact@honk.wang>
2024-07-16 22:18:33 +0800

    delete fish.js

--- background/bg/original/background.js
+++ background/bg/original/background.js
@@ -16,28 +16,6 @@ document.addEventListener('visibilitychange', function () {
     }
 });
 
-// 页脚养鱼
-fish();
-function fish() {
-    return (
-      $("#footer-wrap").css({
-        position: "absolute",
-        "text-align": "center",
-        top: 0,
-        right: 0,
-        left: 0,
-        bottom: 0,
-      }),
-      $("footer").append(
-        '<div class="container" id="jsi-flying-fish-container"></div>'
-      ),
-      $("body").append(
-        '<script src="https://cdn.jsdelivr.net/gh/HonkW93/cdn4blog/background/fish/fish.js"></script>'
-      ),
-      this
-    );
-  }
-  
 /* 手机客户端导航栏默认隐藏 */
 var mobile_sidebar_menus = document.getElementById("sidebar-menus");
 var menus_item_child = mobile_sidebar_menus.getElementsByClassName("menus_item_child");
